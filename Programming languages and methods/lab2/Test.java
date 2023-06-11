d946742378957bb78c8e8973443630ab5151e52f
Egor <69920824+Egorrrad@users.noreply.github.com>
2023-06-11 16:45:47 +0300

    Add files via upload

---
+++ Programming languages and methods/lab2/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Chess Pole=new Chess(10,10);
+        Pole.getPole();
+        Pole.getPosition(1);
+        Pole.changePosition(1,3,1);
+    }
+}
